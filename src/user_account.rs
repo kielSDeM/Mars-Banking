fc75bfabb8a32b7fdd4e1e5d159fc5bdf07b4a74
KielSD <73503331+kielSDeM@users.noreply.github.com>
2021-10-04 15:55:06 +0000

    Update user_account.rs

--- src/user_account.rs
+++ src/user_account.rs
@@ -1,8 +1,7 @@
-pub use crate::new_user::user_info;
 
-pub fn user_account(age: &String) {
-    println!("{:?}", age);
-    println!("Welcome! How may I help you today?");
+
+pub fn user_account() {
+ 
     /*TODO: We need to be able to deposit, withdraw, transfer,
     look at our transaction history and look at our account total.
     */
