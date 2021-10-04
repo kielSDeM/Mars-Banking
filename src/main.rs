8add4cbff41a304d744a62451a7c2fb91b2d4d72
KielSD <73503331+kielSDeM@users.noreply.github.com>
2021-10-04 15:52:41 +0000

    Update main.rs
    updating my code to refelct changes.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
-mod ll;
-mod new_user;
+pub mod new_user;
 mod settings;
 mod user_account;
-
-pub use crate::new_user::bank_new_user;
 pub use crate::settings::settings;
 pub use crate::user_account::user_account;
-
+use new_user::NewUser;
+use new_user::UserInfo;
 fn main() {
-    let age3 = String::new();
-
-    let mut select = String::new();
-    println!("Welcome to Mars Banking!");
-    loop {
-        println!("What would you like to do today?");
-        println!("Create a new account: 1\nLogin: 2\nSettings: 3\nExit: 4");
-        select.clear();
-        std::io::stdin().read_line(&mut select);
-        let select = select.trim();
-        match select {
-            "1" => bank_new_user(),
-            "2" => user_account(&age3),
-            "3" => settings(),
-            "4" => break,
-            _ => {}
+    loop{
+        let mut select = String::new();
+            println!("Welcome to Mars Banking!");
+            println!("What would you like to do today?");
+            println!("Create a new account: 1\nLogin: 2\nSettings: 3\nExit: 4");
+            select.clear();
+            std::io::stdin().read_line(&mut select);
+            let select = select.trim();
+            match select {
+                "1" => NewUser::bank_new_user(self),
+                "2" => user_account(),
+                "3" => settings(),
+                "4" => break,
+                _ => {}
+            }
         }
     }
-}
+
