4c7cb509a1239452e8928b1673958fe919a0f614
KielSD <73503331+kielSDeM@users.noreply.github.com>
2021-11-03 19:51:03 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,25 @@ mod settings;
 mod user_account;
 pub use crate::settings::settings;
 pub use crate::user_account::user_account;
-use new_user::NewUser;
-use new_user::UserInfo;
+use new_user::AccountCreation;
+use new_user::CreateAccount;
 fn main() {
-    loop{
+    loop {
         let mut select = String::new();
-            println!("Welcome to Mars Banking!");
-            println!("What would you like to do today?");
-            println!("Create a new account: 1\nLogin: 2\nSettings: 3\nExit: 4");
-            select.clear();
-            std::io::stdin().read_line(&mut select);
-            let select = select.trim();
-            match select {
-                "1" => NewUser::bank_new_user(self),
-                "2" => user_account(),
-                "3" => settings(),
-                "4" => break,
-                _ => {}
-            }
+        println!("Welcome to Mars Banking!");
+        println!("What would you like to do today?");
+        println!("Create a new account: 1\nLogin: 2\nSettings: 3\nExit: 4");
+        select.clear();
+        std::io::stdin().read_line(&mut select);
+        let select = select.trim();
+        match select {
+            "1" => AccountCreation::bank_new_user(),
+            "2" => user_account(),
+            "3" => settings(),
+            "4" => break,
+            _ => {}
         }
     }
+}
+
 
