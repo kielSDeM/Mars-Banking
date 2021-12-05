4325022e083548b75234a0f08dfbbf13ee56adfc
KielSD <73503331+kielSDeM@users.noreply.github.com>
2021-12-05 01:16:24 -0700

    Update new_user.rs

--- src/new_user.rs
+++ src/new_user.rs
@@ -2,22 +2,16 @@ use rand::Rng;
 //just asks for the users name and age and no more personal info for incase someone wants to check
 //out this repo.
 #[allow(dead_code)]
-pub struct AccountNumber {
-    account_number: i32,
-    routing_number: i32,
-}
-
-pub trait AccountInfo {
-    fn acc_no() -> (i32, i32);
-}
 
 pub struct NewUser {
     age: String,
     new_user: String,
+    account_number: i32,
+    routing_number: i32,
 }
 
 pub trait UserInfo {
-    fn user_info() -> (String, String);
+    fn user_info();
 }
 
 pub trait CreateAccount {
@@ -29,10 +23,12 @@ pub struct AccountCreation {
 }
 //function for user info.
 impl UserInfo for NewUser {
-    fn user_info() -> (String, String) {
+    fn user_info() {
         let mut user_info = NewUser {
             age: String::new(),
             new_user: String::new(),
+            account_number: rand::thread_rng().gen_range(10000000..99999999),
+            routing_number: rand::thread_rng().gen_range(10000000..99999999),
         };
         println!("What is your name?");
         print!("Name: ");
@@ -40,29 +36,19 @@ impl UserInfo for NewUser {
         println!(" ");
         println!("Hello {}, What is your age? ", user_info.new_user);
         std::io::stdin().read_line(&mut user_info.age);
-        user_info.age.trim_end();
-        return (user_info.age, user_info.new_user);
-    }
-}
-impl AccountInfo for AccountNumber {
-    fn acc_no() -> (i32, i32) {
-        let acc_no = AccountNumber {
-            account_number: rand::thread_rng().gen_range(10000000..99999999),
-            routing_number: rand::thread_rng().gen_range(10000000..99999999),
-        };
         println!(
             "We will generate a new account number \
-           and routing number for you."
+               and routing number for you."
         );
-        println!("Your account number is {}", acc_no.account_number);
-        println!("Your account routing number is {}", acc_no.routing_number);
-        return (acc_no.account_number, acc_no.routing_number);
+        println!("Your account number is {}", user_info.account_number);
+        println!(
+            "Your account routing number is {}",
+            user_info.routing_number
+        );
     }
 }
-
 fn yes() {
     NewUser::user_info();
-    AccountNumber::acc_no();
 }
 impl CreateAccount for AccountCreation {
     fn bank_new_user() {
@@ -83,7 +69,7 @@ impl CreateAccount for AccountCreation {
             match account {
                 "yes" => {
                     yes();
-                    break;
+                    continue;
                 }
 
                 "no" => {
